37ed8925fec0a7a4ce9ba64dd4008a19f3bb4193
sumit <sumitsaurav121@gmail.com>
2025-10-26 17:06:55 +0530

    Non blocking IO

--- src/main/java/sec02/Lec10NonBlockingIO.java
+++ src/main/java/sec02/Lec10NonBlockingIO.java
@@ -9,7 +9,7 @@ public class Lec10NonBlockingIO {
     private static final Logger log = LoggerFactory.getLogger(Lec10NonBlockingIO.class);
     public static void main(String[] args) {
         var client = new ExternalServiceClient();
-
+        log.info("Starting...");
         for (int i = 1; i <= 100; i++) {
             client.getProductName(i)
                     .subscribe(Util.subscriber());
