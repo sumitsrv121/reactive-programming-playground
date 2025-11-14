13963a257e8462555c28e05acd14a294a82956a4
sumit <sumitsaurav121@gmail.com>
2025-11-14 21:25:33 +0530

    Multiple subscribeOns

---
+++ src/main/java/sec07/Lec03MultipleSubscribeOn.java
@@ -0,0 +1,33 @@
+package sec07;
+
+import common.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class Lec03MultipleSubscribeOn {
+    private static final Logger log = LoggerFactory.getLogger(Lec03MultipleSubscribeOn.class);
+
+    public static void main(String[] args) {
+        var flux = Flux.create(
+                        sink -> {
+                            for (int i = 0; i < 3; i++) {
+                                log.info("Generating value : {}", i);
+                                sink.next(i);
+                            }
+                            sink.complete();
+                        })
+                .subscribeOn(Schedulers.newParallel("sumit"))
+                .doOnNext(v -> log.info("value: {}", v))
+                .doFirst(() -> log.info("first1"))
+                .subscribeOn(Schedulers.boundedElastic())
+                .doFirst(() -> log.info("first2"));
+
+        Runnable runnable = () -> flux.subscribe(Util.subscriber());
+
+        Thread.ofPlatform().start(runnable);
+
+        Util.sleep(2);
+    }
+}
