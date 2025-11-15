af32f31f66bf6c854b6fca0e0894580c831cf67f
sumit <sumitsaurav121@gmail.com>
2025-11-15 09:19:51 +0530

    publish on

---
+++ src/main/java/sec07/Lec05PublishOn.java
@@ -0,0 +1,33 @@
+package sec07;
+
+import common.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class Lec05PublishOn {
+    private static final Logger log = LoggerFactory.getLogger(Lec05PublishOn.class);
+
+    public static void main(String[] args) {
+        var flux = Flux.create(
+                        sink -> {
+                            for (int i = 0; i < 3; i++) {
+                                log.info("Generating value : {}", i);
+                                sink.next(i);
+                            }
+                            sink.complete();
+                        })
+                .publishOn(Schedulers.parallel())
+                .doOnNext(v -> log.info("value: {}", v))
+                .doFirst(() -> log.info("first1"))
+                .publishOn(Schedulers.boundedElastic())
+                .doFirst(() -> log.info("first2"));
+
+        Runnable runnable = () -> flux.subscribe(Util.subscriber("sub1"));
+
+        Thread.ofPlatform().start(runnable);
+
+        Util.sleep(2);
+    }
+}
