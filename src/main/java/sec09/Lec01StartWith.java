9a9aa8a7bbda583618e382a3c71071d729ef06b5
sumit <sumitsaurav121@gmail.com>
2025-11-16 16:35:46 +0530

    start with operator

---
+++ src/main/java/sec09/Lec01StartWith.java
@@ -0,0 +1,63 @@
+package sec09;
+
+import common.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Lec01StartWith {
+    private static final Logger log = LoggerFactory.getLogger(Lec01StartWith.class);
+    public static void main(String[] args) {
+        demo5();
+        Util.sleep(3);
+    }
+
+    private static void demo1() {
+        producer1()
+                .startWith(-1, 0)
+                .take(2)
+                .subscribe(Util.subscriber());
+    }
+
+    private static void demo2() {
+        producer1()
+                .startWith(List.of(-2, -1, 0))
+                .subscribe(Util.subscriber());
+    }
+
+    private static void demo3() {
+        producer1()
+                .startWith(producer2())
+                .subscribe(Util.subscriber());
+    }
+
+    private static void demo4() {
+        producer1()
+                .startWith(producer2())
+                .startWith(1000)
+                .subscribe(Util.subscriber());
+    }
+
+    // 49, 50, 51, 52, 53, 0, 1, 2, 3
+    private static void demo5() {
+        producer1()
+                .startWith(0)
+                .startWith(producer2())
+                .startWith(49, 50)
+                .subscribe(Util.subscriber());
+    }
+    private static Flux<Integer> producer1() {
+        return Flux.just(1, 2, 3)
+                .doOnSubscribe(subscription -> log.info("subscribing to producer 1"))
+                .delayElements(Duration.ofMillis(10));
+    }
+
+    private static Flux<Integer> producer2() {
+        return Flux.just(51, 52, 53)
+                .doOnSubscribe(subscription -> log.info("subscribing to producer 2"))
+                .delayElements(Duration.ofMillis(10));
+    }
+}
