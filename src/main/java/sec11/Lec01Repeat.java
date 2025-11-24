aa93a4dd4a97f1636f5700eed136739173ea2d1b
sumit <sumitsaurav121@gmail.com>
2025-11-24 10:30:13 +0530

    Repeat

--- src/main/java/sec11/Lec01Repeat.java
+++ src/main/java/sec11/Lec01Repeat.java
@@ -1,18 +1,53 @@
 package sec11;
 
 import common.Util;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Lec01Repeat {
     public static void main(String[] args) {
-        Mono<String> countryMono = Mono
-                .fromSupplier(() -> Util.faker().country().name());
+        demo5();
 
-        var subscriber = Util.subscriber();
+        Util.sleep(5);
+    }
 
-        countryMono
+    private static void demo1() {
+        getCountryName()
                 .repeat(3)
-                .subscribe(subscriber);
+                .subscribe(Util.subscriber());
+    }
 
+    private static void demo2() {
+        getCountryName()
+                .repeat()
+                .takeUntil("Canada"::equalsIgnoreCase)
+                .subscribe(Util.subscriber());
+    }
+
+    private static void demo3() {
+        var atomicInteger = new AtomicInteger(0);
+        getCountryName()
+                .repeat(() -> atomicInteger.incrementAndGet() < 3)
+                .subscribe(Util.subscriber());
+    }
+
+    private static void demo4() {
+        getCountryName()
+                .repeatWhen(flux -> flux.delayElements(Duration.ofMillis(500)).take(2))
+                .subscribe(Util.subscriber());
+    }
+
+    private static void demo5() {
+        Flux.just(1, 2, 3)
+                .repeat(2)
+                .subscribe(Util.subscriber());
+    }
+
+    private static Mono<String> getCountryName() {
+        return Mono
+                .fromSupplier(() -> Util.faker().country().name());
     }
 }
